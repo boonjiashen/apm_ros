d98802e97988c0a771fac5bbedda96ca877cc99b
Jia-Shen Boon <jiashen@gmail.com>
2015-06-17 15:04:30 -0500

    Register keypresses

---
+++ joystick_input.py
@@ -0,0 +1,34 @@
+#importing modules
+import pygame
+from pygame.locals import *
+from sys import exit
+  
+  
+#initializing variables
+pygame.init()
+screen=pygame.display.set_mode((640,480),0,24)
+pygame.display.set_caption("Key Press Test")
+f1=pygame.font.SysFont("comicsansms",24)
+  
+#main loop which displays the pressed keys on the screen
+while True:
+ for i in pygame.event.get():
+  if i.type==QUIT:
+   exit()
+  a=100
+  screen.fill((255,255,255))
+  if pygame.key.get_focused():
+   presses = [pygame.key.name(i) for i, is_pressed in enumerate(pygame.key.get_pressed()) if is_pressed]
+   print presses
+   if 'escape' in presses:
+       exit()
+   #press=pygame.key.get_pressed()
+   #for i in xrange(0,len(press)): 
+    #if press[i]==1:
+     #name=pygame.key.name(i) 
+     #text=f1.render(name,True,(0,0,0))
+     #screen.blit(text,(100,a))
+     #a=a+100
+   pygame.display.update()
+
+
