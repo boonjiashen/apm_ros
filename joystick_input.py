71e6a3699f94717dda68e0e84955b062aacb968f
Jia-Shen Boon <jiashen@gmail.com>
2015-06-18 14:28:39 -0500

    Refactor to make less indented

--- joystick_input.py
+++ joystick_input.py
@@ -1,8 +1,6 @@
 #importing modules
 import pygame
-from pygame.locals import *
-from sys import exit
-  
+from pygame.locals import *  
   
 #initializing variables
 pygame.init()
@@ -12,31 +10,24 @@ f1=pygame.font.SysFont("comicsansms",24)
   
 #main loop which displays the pressed keys on the screen
 while True:
-    for i in pygame.event.get():
-        #print i
-        if i.type==QUIT:
-            exit()
-        a=100
-        screen.fill((255,255,255))
+    event = pygame.event.poll()
+    if event.type == pygame.NOEVENT:
+        continue
+    if event.type == pygame.QUIT:
+        break
+    if not pygame.key.get_focused():
+        continue
 
-        if not pygame.key.get_focused():
-            continue
+    names = [pygame.key.name(i) for i, is_pressed in enumerate(pygame.key.get_pressed()) if is_pressed]
+    if 'escape' in names:
+        break
 
-        names = [pygame.key.name(i) for i, is_pressed in enumerate(pygame.key.get_pressed()) if is_pressed]
-        #print names
-        if 'escape' in names:
-            exit()
-        ys = [100 * i for i in range(1, 1 + len(names))]
-        for name, y in zip(names, ys):
-            text = f1.render(name, True, (0, 0, 0))
-            screen.blit(text, (100, y))
-        # press=pygame.key.get_pressed()
-        # for i in xrange(0,len(press)): 
-        #     if press[i]==1:
-        #         name=pygame.key.name(i) 
-        #         text=f1.render(name,True,(0,0,0))
-        #         screen.blit(text,(100,a))
-        #         a=a+100
-        pygame.display.update()
+    ys = [100 * i for i in range(1, 1 + len(names))]
+    screen.fill((255,255,255))
+    for name, y in zip(names, ys):
+        text = f1.render(name, True, (0, 0, 0))
+        screen.blit(text, (100, y))
+
+    pygame.display.update()
           
           
